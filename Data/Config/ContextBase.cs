a1565de35ca00645f306b1f262dcad7c166a7205
Joice Nunes <joiccarnunes@gmail.com>
2023-02-06 17:55:06 -0300

    Adicionar arquivos de projeto.

---
+++ Data/Config/ContextBase.cs
@@ -0,0 +1,50 @@
+﻿using Data.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Config
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        { }
+
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+            base.OnModelCreating(builder);
+        }
+
+
+        private string GetStringConectionConfig()
+        {
+            string strCon = "Data Source=DESKTOP-KU29BD2\\DESENVOLVIMENTO;Initial Catalog=Sistema_Produto;Integrated Security=False;User ID=mvc;Password=123456";
+
+            return strCon;
+        }
+
+    }
+}
+
+
+
+
+   
